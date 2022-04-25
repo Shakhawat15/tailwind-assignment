f9d5ae6595a7cc3724bd46867f22f8d657b7f9cc
Shakhawat <shsourov2000@gmail.com>
2022-04-26 05:11:13 +0600

    design dashboard, login, register, and password reset paged

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Nunito': ['Nunito', 'sans-serif'],
+      },
+      backgroundImage: {
+        'main': "url('./../images/bg-pattern-light.svg')",
+      },
+    },
+  },
+  plugins: [],
+}
